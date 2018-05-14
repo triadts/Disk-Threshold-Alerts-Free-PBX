bec62c3b3ac609fc68035e1981258b3f3c048e91
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-05-14 11:09:29 -0400

    Update Master.sh
    Updated operators in if statement

--- Master.sh
+++ Master.sh
@@ -1,7 +1,7 @@
 CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
 THRESHOLD=80
 
-if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
+if [ "$CURRENT" >= -gt "$THRESHOLD" ] ; then
     mail -r <companynamePBX> -s 'Disk Space Alert' recipient@domain.com << EOF
 Your root partition remaining free space is critically low. Used: $CURRENT%
 EOF

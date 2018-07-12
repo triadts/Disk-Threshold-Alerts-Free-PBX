4cb38af14f98af3c9d63021a50e87aaeedba39ff
Keith Miller <36780654+Keimille@users.noreply.github.com>
2018-07-12 16:19:20 -0400

    Update filetruncate.sh

--- filetruncate.sh
+++ filetruncate.sh
@@ -3,11 +3,13 @@
 #set log files to empty themselves either periodically and/or when they reach a threshold
 
 #this will identify all files
-CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's%//g')
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+#TRUNCATE=$(truncate -s 0 /var/log/asterisk/*)
 #THRESHOLD=70
 
 if [ "$CURRENT" > 80 ] | [ "$CURRENT"  = 80 ] ; then
+#do TRUNCATE
 #this should only erase log files that have reached the threshold
-truncate -s 0 /var/log/asterisk/*
+    alias proj="truncate -s 0 /var/log/asterisk/*"
 fi
 
